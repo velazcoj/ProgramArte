a74bdc57c038955696f2d1a5b7ecb6ec2bd177a0
Lucas Boned <83147215+Lucas-Ezequiel@users.noreply.github.com>
2024-09-26 14:14:51 -0300

    Update Segunda Entrega - Codigo Preliminar.py

--- Segunda Entrega - Codigo Preliminar.py
+++ Segunda Entrega - Codigo Preliminar.py
@@ -6,9 +6,6 @@ class Generador:
     def __init__(self, contenido_inicial):
         self.contenidoInicial = contenido_inicial
         
-    # @abstractmethod   
-    # def generar(apartir_de):
-    #     pass
 
     @abstractmethod
     def generar(self, apartir_de):
@@ -28,6 +25,8 @@ class Generador:
 class GeneradorTexto(Generador): #objeto de la clase generador
     def __init__(self, contenido_inicial=""): 
         super().__init__(contenido_inicial)
+
+    
     def generar(self, apartir_de):
         texto_generado = "Hola, esto es un texto generado"
         return texto_generado
@@ -35,7 +34,7 @@ class GeneradorTexto(Generador): #objeto de la clase generador
 
 
     
-class FrAI(Frame):
+class FrameIA(Frame):
 
     def __init__(self, master=None):
         super().__init__(master,width=720, height=570)
@@ -52,18 +51,18 @@ class FrAI(Frame):
     #     self.txtNum3.delete("1.0", "end")  
     #     self.txtNum3.insert("1.0", f" aando descalza, por las calles soleadas de mi música\n soltando pájaros blancos de mis manos\n besando las pupilas negras de soles amarillos\n ando descalza, sobre pasto tierno y acordes azulados")
  
-    def fEnviar(self):
+    def enviar(self):
         n1 = self.txtNum1.get("1.0", "end-1c")  # Obtiene el texto del input
         self.txtNum1.delete("1.0", "end")
         self.txtNum3.delete("1.0", "end")  
         resultado = self.generador.generar(n1)  # Genera texto basado en el input
-        self.txtNum3.insert("1.0", resultado)  # Inserta el resultado en el Text de salida
+        self.txtNum3.insert("1.0", resultado)  # Inserta el resultado en la caja de texto de respuesta
 
 
 
 
     def create_widgets(self):
-
+        #Creacion de menu, posiciones, colores...
         
         self.lblTitle = Label(self,text="ProgramArte", fg="black",font= "consolas 20 bold",
         bd=2,
@@ -72,9 +71,9 @@ class FrAI(Frame):
         self.lblNum1 = Label(self,text="Escribe Algo: ",bg="white")
         self.txtNum1=Text(self,bg="white", height=1)
         
-        self.btn1=Button(self,text="Enviar", command=self.fEnviar)
+        self.btn1=Button(self,text="Enviar", command=self.Enviar)
         
-        self.lblNum3 = Label(self,text="AI dice:",bg="yellow")
+        self.lblNum3 = Label(self,text="IA dice:",bg="yellow")
         self.txtNum3=Text(self,bg="cyan")
         self.lblTitle.place(x=10,y=10,width=700, height=100)
         
