2d56357e459b32d4433088c2cb46894b69ecbd3b
Lucas Boned <83147215+Lucas-Ezequiel@users.noreply.github.com>
2024-09-26 19:24:44 -0300

    Update Segunda Entrega - Codigo Preliminar.py

--- Segunda Entrega - Codigo Preliminar.py
+++ Segunda Entrega - Codigo Preliminar.py
@@ -6,23 +6,35 @@ class Generador:
     def __init__(self, contenido_inicial):
         self.contenidoInicial = contenido_inicial
         
-    @abstractmethod   
-    def generar(apartir_de):
+
+    @abstractmethod
+    def generar(self, apartir_de):
         pass
+
+
+
+# class GeneradorTexto(Generador): #objeto de la clase generador
+#     def __init__ (self):
+#         pass
+#     def generar(self, apartir_de):
+        
+#         texto_generado = "Hola, esto es un texto generado"
+#         return texto_generado
 
 
 class GeneradorTexto(Generador): #objeto de la clase generador
-    def __init__ (self):
-        pass
+    def __init__(self, contenido_inicial=""): 
+        super().__init__(contenido_inicial)
+
+    
     def generar(self, apartir_de):
-        
-        texto_generado = "Hola, esto es un texto generado" #Aca funccionaria la API (Interfaz)
+        texto_generado = "Hola, esto es un texto generado"
         return texto_generado
 
 
 
     
-class FrAI(Frame):
+class FrameMenuPrincipal(Frame):
 
     def __init__(self, master=None):
         super().__init__(master,width=720, height=570)
@@ -32,31 +44,41 @@ class FrAI(Frame):
         self.generador = GeneradorTexto()
         
 
-    def fEnviar(self):
+    # def fEnviar(self):
          
-        n1 = self.txtNum1.get("1.0", "end-1c")
+    #     n1 = self.txtNum1.get("1.0", "end-1c")
+    #     self.txtNum1.delete("1.0", "end")
+    #     self.txtNum3.delete("1.0", "end")  
+    #     self.txtNum3.insert("1.0", f" aando descalza, por las calles soleadas de mi música\n soltando pájaros blancos de mis manos\n besando las pupilas negras de soles amarillos\n ando descalza, sobre pasto tierno y acordes azulados")
+ 
+    def enviar(self):
+        textoingresado = self.txtNum1.get("1.0", "end-1c")  # Obtiene el texto del input
         self.txtNum1.delete("1.0", "end")
         self.txtNum3.delete("1.0", "end")  
-        self.txtNum3.insert("1.0", f" aando descalza, por las calles soleadas de mi música\n soltando pájaros blancos de mis manos\n besando las pupilas negras de soles amarillos\n ando descalza, sobre pasto tierno y acordes azulados")
- 
+        resultado = self.generador.generar(textoingresado)  # Genera texto basado en el input
+        self.txtNum3.insert("1.0", resultado)  # Inserta el resultado en la caja de texto de respuesta
+
+
+
 
     def create_widgets(self):
-
+        #Creacion de menu, posiciones, colores...
         
-        self.lblTitle = Label(self,text="ProgramArte", fg="black",font= "consolas 20 bold",
-        bd=2,
-        padx=10, pady=10)
+        self.lblTitle = Label(self,text="ProgramArte", fg="black",font= "consolas 20 bold", bd=2,padx=10, pady=10)
         self.lblTitle.pack( padx=10, pady=10)
+        
         self.lblNum1 = Label(self,text="Escribe Algo: ",bg="white")
         self.txtNum1=Text(self,bg="white", height=1)
         
-        self.btn1=Button(self,text="Enviar", command=self.fEnviar)
-        
-        self.lblNum3 = Label(self,text="AI dice:",bg="yellow")
+        self.btn1=Button(self,text="Enviar", command=self.enviar)
+
+        self.lblNum3 = Label(self,text="IA dice:",bg="yellow")
         self.txtNum3=Text(self,bg="cyan")
+
+        
+        
         self.lblTitle.place(x=10,y=10,width=700, height=100)
         
-
         
         self.lblNum1.place(x=20,y=200,width=100, height=10)
         self.txtNum1.place(x=120,y=200,width=200, height=30)
@@ -70,6 +92,5 @@ class FrAI(Frame):
 
 root = Tk()
 root.wm_title("ProgramArte")
-app = FrAI(root) 
+app = FrameMenuPrincipal(root) 
 app.mainloop()
-
